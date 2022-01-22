32c101063e92a1d73ec52a3b3dd8f482e528e052
ilanma2906 <75073457+ilanma2906@users.noreply.github.com>
2022-01-22 20:16:42 +0200

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCupOverflows.Models;
+using MyCupOverflows.ViewModel;
+using MyCupOverflows.Dal;
+using System.Data.Entity;
+using System.IO;
+
+namespace MyCupOverflows.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayOrders(OrdersViewModel model)
+        {
+            OrdersDal dal = new OrdersDal();
+            OrdersViewModel mvm = new OrdersViewModel();
+            List<Orders> ticketss = dal.OrdersList.ToList();
+            mvm.Orders = new Orders();
+            mvm.OrdersList = ticketss;
+            return View(mvm);
+        }
+
+        [HttpPost]
+        public ActionResult ManageProduct()
+        {
+            OrdersDal dal = new OrdersDal();
+            if (ModelState.IsValid)
+            {
+                var data = dal.OrdersList.ToList();
+                return View();
+            }
+            else
+
+                return View("Orders/DisplayOrders");
+        }
+    }
+}
