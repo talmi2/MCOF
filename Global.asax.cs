49d6e51a6deb42d3609a5c9f99634194314de0a5
ilanma2906 <75073457+ilanma2906@users.noreply.github.com>
2022-01-22 20:21:56 +0200

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +7,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MyCupOverflows.Dal;
+using System.Web.Configuration;
+using Stripe;
 
-namespace MCOF
+namespace MyCupOverflows
 {
     public class MvcApplication : System.Web.HttpApplication
     {
@@ -16,6 +21,14 @@ namespace MCOF
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var secretKey = WebConfigurationManager.AppSettings["StripeSecretKey"];
+            StripeConfiguration.ApiKey = secretKey;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add application services.
+            //services.AddDbContext<UserDal>
         }
     }
 }
