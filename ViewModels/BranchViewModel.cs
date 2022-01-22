a6a7c2d069101775754bdb6ba79d53a00ea4e604
ilanma2906 <75073457+ilanma2906@users.noreply.github.com>
2022-01-22 20:18:51 +0200

    Add files via upload

---
+++ ViewModels/BranchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyCupOverflows.Models;
+
+namespace MyCupOverflows.ViewModel
+{
+    public class BranchViewModel
+    {
+        public Branch Branch { get; set; }
+        public List<Branch> Branches { get; set; }
+    }
+}
---
+++ ViewModels/ReservationCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyCupOverflows.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCupOverflows.ViewModel
+{
+    public class ReservationCartViewModel
+    {
+        public ReservationCart ReservationCart { get; set; }
+        public List<ReservationCart> ITEMS { get; set; }
+    }
+}
